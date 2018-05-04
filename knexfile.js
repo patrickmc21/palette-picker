434d2e1f92d13b824811306cb382f91c8d1e15c5
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-04 10:19:43 -0600

    Update for production

--- knexfile.js
+++ knexfile.js
@@ -42,19 +42,12 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  client: 'pg',
+  connection: process.env.DATABASE_URL + `?ssl=true`,
+  migrations: {
+    directory: './migrations'
+  },
+  useNullAsDefault: true
+}
 
 };
