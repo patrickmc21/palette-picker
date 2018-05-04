1406a4df7a774a272575ccfbcc6c525eee76d0fe
Patrick McLaughlin <patrickmc21@gmail.com>
2018-05-04 12:00:41 -0600

    comment server.js

--- server.js
+++ server.js
@@ -79,43 +79,66 @@ app.post('/api/v1/projects', (req, res) => {
 
 // set POST route for palettes
 app.post('/api/v1/projects/:project_id/palette', (req, res) => {
+  // destructure project id from params
   const { project_id } = req.params;
+  // assign request body to variable palette
   const palette = req.body;
+  // destructure palette
   const { name, color1, color2, color3, color4, color5 } = palette;
+  // conditional for making sure body includes correct key/value pairs
   if (name && color1 && color2 && color3 && color4 && color5) {
+    // insert palette with project id into palettes table, return palette id
     db('palettes').insert({...palette, project_id}, 'id')
+    // use then() method to handle promise return from db
       .then(palette => {
+        // send response with status 201 and send palette id
         res.status(201).json({id: palette[0] })
       })
+      // set catch for error handling from db return
       .catch(error => {
-        res.status(500).send('Error posting palette');
+        // send response with status 500 and message
+        res.status(500).json({message: 'Error posting palette'});
       });
+      // conditional if request body does not include correct key/value pairs
   } else {
+    // send response with status 422 and error message
     res.status(422).send({error: 'Please include a valid palette'});
   }
 });
 
+// set DELETE route for projects
 app.delete('/api/v1/projects/:id', (req, res) => {
+  // destructure id dfrom params
   const { id } = req.params;
-
+  // delete all from projects table where project id matches params id
   db('projects').where('id', id).del()
+  // send response with status 204 on successful delete
     .then(id => res.sendStatus(204))
+    // send response with status 404 if problem with delete
     .catch(error => res.status(404).send({message: 'project not found'}));
 });
 
+// set DELETE route for palettes
 app.delete('/api/v1/palette/:id', (req, res) => {
+  // destructure id from request params
   const { id } = req.params;
+  // delete all from table palettes where palette id matches params id
   db('palettes').where({id: id}).del()
+  // send response with status 204 on successful delete
     .then(palette => {
       res.sendStatus(204)
     })
+    // send status 404 and message on unsuccessful delete
     .catch(error => {
       res.status(404).json({message:'invalid id'})
     });
 });
 
+// set port for server to listen on using environent variable PORT
 app.listen(app.get('port'), () => {
+  // log a message to the console telling what port the app is running on
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
+// export server and database so they can be tested
 module.exports = { app, db };
